7c1d2c281d5e95730beaa8e9a488b9e1edde1297
Robert McCreary Mannino <fooyinfighter@gmail.com>
2025-10-02 11:31:10 -0700

    add mini-project-portfolio

---
+++ week2/mini-project-portfolio/src/App.jsx
@@ -0,0 +1,62 @@
+import './App.css'
+import HeroImage from './components/HeroImage'
+
+function App() {
+
+  return (
+    <>
+      <HeroImage />
+      {/* MAIN (Center website) */}
+      <div className="main">
+
+        <h1>MYLOGO.COM</h1>
+        <hr />
+
+        <h2>PORTFOLIO</h2>
+        <p>Resize the browser window to see the responsive effect.</p>
+
+        {/* Portfolio Gallery Grid */}
+        <div className="row">
+          <div className="column">
+            <div className="content">
+              <img src="/mountains.jpg" alt="Mountains" className="project-image" />
+              <h3>My Work</h3>
+              <p>Lorem ipsum..</p>
+            </div>
+          </div>
+          <div className="column">
+            <div className="content">
+              <img src="/lights.jpg" alt="Lights" className="project-image" />
+              <h3>My Work</h3>
+              <p>Lorem ipsum..</p>
+            </div>
+          </div>
+          <div className="column">
+            <div className="content">
+              <img src="/nature.jpg" alt="Nature" className="project-image" />
+              <h3>My Work</h3>
+              <p>Lorem ipsum..</p>
+            </div>
+          </div>
+          <div className="column">
+            <div className="content">
+              <img src="/mountains.jpg" alt="Mountains" className="project-image" />
+              <h3>My Work</h3>
+              <p>Lorem ipsum..</p>
+            </div>
+          </div>
+        </div>
+
+        <div className="content">
+          <img src="/bear.jpg" alt="Bear" className="project-image" />
+          <h3>Some Other Work</h3>
+          <p>Lorem ipsum..</p>
+        </div>
+
+        {/* END MAIN */}
+      </div>
+    </>
+  )
+}
+
+export default App
