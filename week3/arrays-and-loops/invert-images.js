da32a392113eb9445573d2355251ea5efcc2358c
Robert McCreary Mannino <fooyinfighter@gmail.com>
2025-10-08 17:26:58 -0700

    update demo code

--- week3/arrays-and-loops/invert-images.js
+++ week3/arrays-and-loops/invert-images.js
@@ -1,3 +1,5 @@
+// run this command to run the program
+// node invert-images.js
 /**
  * 
  * @param {Array} imageList A list with the pixels to display
@@ -19,14 +21,16 @@ function printImage(imageList, width, height) {
 // This string of a dog is from: https://studio.code.org/courses/csd-2025/units/6/lessons/4/levels/3
 const dogString = "111111111111101111111111101111100111101111100001101111100001110000000111110000000111110000000111110111110111110111110111110111110111111111111111";
 const dogList = dogString.split("");
+console.log("dog image...");
 printImage(dogList, 12, 12);
 
 const invertedDogList = [];
 for(let i=0; i<dogList.length; i++) {
     if(dogList[i]== "0") {
-        invertedDogList.push(1)
-    } else {
-        invertedDogList.push(0)
+        invertedDogList[i] = "1"
+    } else if(dogList[i] == "1") {
+        invertedDogList[i] = "0"
     }   
 }
+console.log("inverted...");
 printImage(invertedDogList, 12, 12);
